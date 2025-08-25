215a2ae4eecfbb0dccd46385abc6607d4d33fad6
Prashant Bhore <prashant.b@mplussoft.in>
2025-08-25 12:20:31 +0530

    vite-config-change

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// export default defineConfig({
+//   base: './', // ← add this
+//   plugins: [react()],
+//   server: {
+//     host: "0.0.0.0", // Listen on all network interfaces
+//     port: 4000, // Or any port you prefer
+//   },
+// });
+
+
 export default defineConfig({
-  base: './', // ← add this
+  base: "/", // ✅ ABSOLUTE PATH! Works with BrowserRouter
   plugins: [react()],
   server: {
-    host: "0.0.0.0", // Listen on all network interfaces
-    port: 4000, // Or any port you prefer
+    host: "0.0.0.0",
+    port: 4000,
+  },
+  optimizeDeps: {
+    exclude: ["@reduxjs/toolkit"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    outDir: "dist", // output folder for build
   },
 });
